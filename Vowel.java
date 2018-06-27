d608dca3a0e943afd70e72ab26697a5e1d041ac2
divyasundharam <divyasundharam8@gmail.com>
2018-06-27 04:23:15 +0000

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class Vowel
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2="aeiou";
+		String s3="bcdfghjklmnpqrstvwxyz";
+		if(s2.contains(s1))
+		{
+			System.out.print("vowel");
+		}
+		else if(s3.contains(s1))
+		{
+			System.out.print("constant");
+		}
+		else
+		{
+			System.out.print("invalid");
+		}
+    }
+    }
