672dbee98cda96845bd1531ad02d5267562f953a
divyasundharam <divyasundharam8@gmail.com>
2018-06-27 03:58:23 +0000

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+class Even
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+		int a=-1;
+		if(a>=1&&a<=100000)
+		{
+			if(a%2==0)
+			{
+				System.out.print("even");
+			}
+			else
+			{
+				System.out.print("odd");
+			}
+		}
+		else
+		{
+			System.out.print("invalid");
+		}
+    }
+    }
+		
