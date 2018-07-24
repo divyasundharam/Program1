d75657b0089854817f9429000968074b0feff0d9
divyasundharam <divyasundharam8@gmail.com>
2018-07-24 21:34:28 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Hello
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s="Hello";
+int a=sc.nextInt();
+for(int i=0;i<a;i++)
+{
+System.out.println(s);
+}
+}
+}
