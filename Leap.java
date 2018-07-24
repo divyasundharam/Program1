d1a7292fd5ce6a7601c81f09fd2e2d386c6e5475
divyasundharam <divyasundharam8@gmail.com>
2018-07-24 21:28:22 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Leap
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%4==0)
+{
+System.out.print("yes");
+}
+else
+{
+System.out.print("no");
+}
+}
+}
