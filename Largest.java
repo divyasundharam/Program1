ae84ce3ce83b825232913e7eb49c425cb8f85747
divyasundharam <divyasundharam8@gmail.com>
2018-07-24 21:23:05 +0530

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -6,11 +6,11 @@ public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
-int b=sc=nextInt();
+int b=sc.nextInt();
 int c=sc.nextInt();
-if(a>b)
+if(a>=b)
 {
-if(a>c)
+if(a>=c)
 {
 System.out.print(a);
 }
@@ -19,9 +19,9 @@ else
 System.out.print(c);
 }
 }
-else if(b>a)
+else if(b>=a)
 {
-if(b>c)
+if(b>=c)
 {
 System.out.print(b);
 }
