1e1cfcc666e841b398714680b4483c7a9a1022ac
divyasundharam <divyasundharam8@gmail.com>
2018-07-29 10:34:40 +0530

    Create Multiple.java

---
+++ Multiple.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Multiple
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(i*a);
+		}
+	}
+}
