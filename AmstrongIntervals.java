47c41ee04c0fe7b551d8c9c7e9bc14b63770e868
divyasundharam <divyasundharam8@gmail.com>
2018-07-29 10:21:35 +0530

    Update AmstrongIntervals.java

--- AmstrongIntervals.java
+++ AmstrongIntervals.java
@@ -8,16 +8,16 @@ public static void main(String args[])
 Scanner sc=new Scanner(System.in);
 int a=sc.nextInt();
 int b=sc.nextInt();
-int sum=0;
+int sum;
 int d=0;
 for(int i=a+1;i<b;i++)
 {
 String s=String.valueOf(i);
+sum=0;
 for(int j=0;j<s.length();j++)
 {
 int b1=Integer.parseInt(String.valueOf(s.charAt(j)));
 sum=sum+(int)Math.pow(b1,3);
-d=1;
 }
 if(i==sum)
 {
