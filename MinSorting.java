1d56b765955311f8ef03655c9f29da9e0acc380c
divyasundharam <divyasundharam8@gmail.com>
2018-08-21 20:26:28 +0530

    Create MinSorting.java

---
+++ MinSorting.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class MinSorting
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+Arrays.sort(a);
+System.out.print(a[0]);
+}
+}
