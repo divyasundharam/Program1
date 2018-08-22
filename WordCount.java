5b64f0011493917200d9a68a631bfa37ba1bea5f
divyasundharam <divyasundharam8@gmail.com>
2018-08-22 18:43:50 +0530

    Create WordCount.java

---
+++ WordCount.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class WordCount
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.nextLine();
+String st[]=s1.split(" ");
+System.out.print(st.length);
+}
+}
