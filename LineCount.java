215ba947b5f926b0a81698fd7bc7c4fe82f6a9ac
divyasundharam <divyasundharam8@gmail.com>
2018-08-22 19:06:19 +0530

    Create LineCount.java

---
+++ LineCount.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class LineCount
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.nextLine();
+String st[]=s1.split(".");
+System.out.print(st.length);
+}
+}
