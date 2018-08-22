8488125941887f6810082775059e93267c75431e
divyasundharam <divyasundharam8@gmail.com>
2018-08-22 21:46:54 +0530

    Create KWords.java

---
+++ KWords.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class KWords
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.nextLine();
+int k=sc.nextInt();
+for(int i=0;i<k;i++)
+{
+System.out.println(s1);
+}
+}
+}
