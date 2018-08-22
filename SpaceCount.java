488a6af48fde541ec9889d04fa65a93abda22588
divyasundharam <divyasundharam8@gmail.com>
2018-08-22 18:59:48 +0530

    Create SpaceCount.java

---
+++ SpaceCount.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class SpaceCount
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.nextLine();
+int count=0;
+for(int i=0;i<s1.length();i++)
+{
+if(s1.charAt(i)=' ')
+{
+count++;
+}
+}
+System.out.print(count);
+}
+}
